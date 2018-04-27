6cbe2f74211cd56905f100f21e25bdb3514b9c1b
zhaomy07 <zhaomy07@vanke.com>
2018-04-27 17:30:31 +0800

    提交本地

--- src/main/java/com/common/realm/HibernateRealm.java
+++ src/main/java/com/common/realm/HibernateRealm.java
@@ -1,5 +1,7 @@
 package com.common.realm;
 
+import java.util.List;
+
 import org.apache.shiro.authc.AuthenticationException;
 import org.apache.shiro.authc.AuthenticationInfo;
 import org.apache.shiro.authc.AuthenticationToken;
@@ -14,6 +16,12 @@ import org.apache.shiro.subject.PrincipalCollection;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import com.common.dao.RolePermissionDAO;
+import com.common.dao.UserDAO;
+import com.common.dao.UserRoleDAO;
+import com.common.pojo.Role;
+import com.common.pojo.User;
+
 /**
  * Configured Apache Shiro Realm.
  */
@@ -21,8 +29,14 @@ import org.springframework.stereotype.Component;
 @Component
 public class HibernateRealm extends AuthorizingRealm {
 
-	/*@Autowired
-	protected UserDao userDao;*/
+	@Autowired
+	private UserDAO userDAO;
+	
+	@Autowired
+	private UserRoleDAO userRoleDAO;
+	
+	@Autowired
+	private RolePermissionDAO rolePermissionDAO;
 
 	public HibernateRealm() {
 		// This name must match the name in the User class's getPrincipals() method
@@ -36,17 +50,17 @@ public class HibernateRealm extends AuthorizingRealm {
 	 */
 	protected AuthenticationInfo doGetAuthenticationInfo(AuthenticationToken authcToken)
 			throws AuthenticationException {
+		// 测试冲突
 		// UsernamePasswordToken对象用来存放提交的登录信息
 		UsernamePasswordToken token = (UsernamePasswordToken) authcToken;
 		// 查出是否有此用户
-		/*User user = userDao.findUser(token.getUsername());
+		User user = userDAO.selectByUsername(token.getUsername());
 		if (user != null) {
 			// 若存在，将此用户存放到登录认证info中
 			return new SimpleAuthenticationInfo(user.getId(), user.getPassword(), getName());
 		} else {
 			return null;
-		}*/
-		return null;
+		}
 	}
 
 	 /** 
@@ -56,19 +70,23 @@ public class HibernateRealm extends AuthorizingRealm {
 		//获取登录时输入的id
 		Long userId = (Long) principals.fromRealm(getName()).iterator().next();
 		//到数据库查是否有此对象  
-		/*User user = userDao.getUser(userId);
+		User user = userDAO.selectByPrimaryKey(userId);
 		if (user != null) {
 			//权限信息对象info,用来存放查出的用户的所有的角色（role）及权限（permission）
 			SimpleAuthorizationInfo info = new SimpleAuthorizationInfo();
-			for (Role role : user.getRoles()) {
-				info.addRole(role.getName());
-				info.addStringPermissions(role.getPermissions());
+			List<Role> list = userRoleDAO.selectRolesByUserId(userId);
+			if (list != null && !list.isEmpty()) {
+				for (Role role : list) {
+					info.addRole(role.getName());
+					info.addStringPermissions(rolePermissionDAO.selectPermissionsByRoleId(role.getId()));
+				}
+				return info;
+			} else {
+				return null;
 			}
-			return info;
 		} else {
 			return null;
-		}*/
-		return null;
+		}
 	}
 
 }
