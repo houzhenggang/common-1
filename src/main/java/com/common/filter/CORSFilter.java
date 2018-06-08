47dcdbe61614c35b2bf5e602e551cbd366fa203b
zhaomingyang9 <zhaomingyang9@crland.com.cn>
2018-06-08 11:28:16 +0800

    修改注释

--- src/main/java/com/common/filter/CORSFilter.java
+++ src/main/java/com/common/filter/CORSFilter.java
@@ -1,10 +1,3 @@
-/*
- * @(#)CorsFilter.java 2017年5月27日
- * 
- * Copyright (c), 2016 深圳市万睿智能科技有限公司（Shenzhen Wan Rui Intelligent Technology Co., Ltd.）
- * 
- * 著作权人保留一切权利，任何使用需经授权。
- */
 package com.common.filter;
 
 import java.io.IOException;
